354c54b5289864b63eddcad2e456fe430cf3ac5b
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2024-07-02 15:16:53 -0400

    updated from 8bitworkshop.com

--- hello.c
+++ hello.c
@@ -11,13 +11,37 @@ Finally, turn on the PPU to display video.
 #include <stdlib.h>
 #include <_heap.h>
 #include <string.h>
-//extern unsigned*          __heaporg;
+#include <peekpoke.h>
 
 #define NES_MAPPER 4		// Mapper 4 (MMC3)
 #define NES_PRG_BANKS 4		// # of 16KB PRG banks
 #define NES_CHR_BANKS 1		// # of 8KB CHR banks
 //#resource "nesbanked.cfg"
 #define CFGFILE nesbanked.cfg
+
+#define MMC_MODE 0x00
+
+#define MMC3_SET_REG(r,n)\
+	POKE(0x8000, MMC_MODE|(r));\
+	POKE(0x8001, (n));
+
+#define MMC3_CHR_0000(n) MMC3_SET_REG(0,n)
+#define MMC3_CHR_0800(n) MMC3_SET_REG(1,n)
+#define MMC3_CHR_1000(n) MMC3_SET_REG(2,n)
+#define MMC3_CHR_1400(n) MMC3_SET_REG(3,n)
+#define MMC3_CHR_1800(n) MMC3_SET_REG(4,n)
+#define MMC3_CHR_1C00(n) MMC3_SET_REG(5,n)
+#define MMC3_PRG_8000(n) MMC3_SET_REG(6,n)
+#define MMC3_PRG_A000(n) MMC3_SET_REG(7,n)
+
+#define MMC3_MIRROR(n) POKE(0xa000, (n))
+
+//; WRAM_OFF $40
+//; WRAM_ON $80
+//; WRAM_READ_ONLY $C0
+#define MMC3_WRAM_DISABLE() POKE(0xA001, 0x40)
+#define MMC3_WRAM_ENABLE() POKE(0xA001, 0x80)
+#define MMC3_WRAM_READ_ONLY() POKE(0xA001, 0xC0)
 
 
 void heap_avail(void)
@@ -54,6 +78,7 @@ void heap_avail(void)
 
 // main function, run after console reset
 void main(void) {
+  MMC3_WRAM_ENABLE();
   // set palette colors
   pal_col(0,0x02);	// set screen to dark blue
   pal_col(1,0x14);	// fuchsia
