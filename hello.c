e8ec63e16a179461171bfd73f8b5cf37a77c95c1
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2024-10-22 20:34:05 -0400

    updated from 8bitworkshop.com

--- hello.c
+++ hello.c
@@ -164,7 +164,7 @@ char pad;
 void main(void) {
   
   
-  while (1)
+  while (0)
   {};
   //ppu_on_all();
   
@@ -175,7 +175,7 @@ void main(void) {
   //ppu_off();  
 
   // set palette colors
-  //pal_col(0,0x02);	// set screen to dark blue
+  pal_col(0,0x02);	// set screen to dark blue
   pal_col(1,0x14);	// fuchsia
   pal_col(2,0x20);	// grey
   pal_col(3,0x30);	// white
